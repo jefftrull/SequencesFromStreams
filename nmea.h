37b4ebba1d8687838c1f5026d624e8faa12661fd
Jeff Trull <edaskel@att.net>
2015-05-12 09:29:53 -0700

    repair incorrect syntax in NMEA comment

--- nmea.h
+++ nmea.h
@@ -5,7 +5,7 @@
 #include <boost/optional.hpp>
 
 // NMEA "sentence" describing GPS coordinates
-// $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
+// $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,,*47
 struct gga {
     int                  timestamp;
     double               latitude;
