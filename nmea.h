fb9846fd3267b7824bf0fc1dcc6715818fb8008b
Jeff Trull <edaskel@att.net>
2015-05-12 12:15:44 -0700

    introduce Spirit parser for NMEA data and demonstrate it can be used in the same way as the stream operator via manipulator adaptation

--- nmea.h
+++ nmea.h
@@ -3,6 +3,7 @@
 #include <iostream>
 
 #include <boost/optional.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
 
 // NMEA "sentence" describing GPS coordinates
 // $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,,*47
@@ -27,3 +28,24 @@ struct gga {
 // declare stream operators
 std::istream& operator>>(std::istream&, gga&);
 std::ostream& operator<<(std::ostream&, gga const&);
+
+// adapt gga to make it accessible to Fusion
+BOOST_FUSION_ADAPT_STRUCT(
+    gga,
+    (int,    timestamp)
+    (double, latitude)
+    (char,   lat_hemi)
+    (double, longitude)
+    (char,   long_hemi)
+    (int,    quality)
+    (int,    sat_count)
+    (double, dilution)
+    (double, altitude)
+    (char,   alt_units)
+    (double, sea_level)
+    (char,   sea_level_units)
+    (boost::optional<int>, time_since_dgps)
+    (boost::optional<int>, dgps_station_id)
+    (int,    checksum)
+)
+    
---
+++ spirit_stream_operator.cpp
@@ -0,0 +1,51 @@
+// Demonstrating the use of ranges built using a qi::spirit parser adapted to be a stream operator
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/qi_match.hpp>
+
+#include <range/v3/all.hpp>
+
+#include "nmea.h"
+
+// supply our own stream operators here that match the declarations
+std::istream&
+operator>>(std::istream& is, gga& g) {
+    using namespace boost::spirit;
+
+    qi::rule<istream_iterator, gga()> gga_parser =
+        qi::lit("$GPGGA") >> ',' >> qi::int_ >> ',' >>
+        qi::double_ >> ',' >> qi::char_("NS") >> ',' >>
+        qi::double_ >> ',' >> qi::char_("EW") >> ',' >>
+        qi::int_ >> ',' >> qi::int_ >> ',' >>
+        qi::double_ >> ',' >>
+        qi::double_ >> ',' >> qi::char_ >> ',' >>
+        qi::double_ >> ',' >> qi::char_ >> ',' >>
+        -qi::int_ >> ',' >> -qi::int_ >> ",*" >> qi::int_ >> -qi::eol;
+        
+    // construct a stream manipulator from this parser
+    auto manip = qi::match(gga_parser, g);
+    is >> manip;
+
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, gga const& g) {
+    os << '(' << g.latitude << g.lat_hemi << ',' << g.longitude << g.long_hemi << ')';
+    return os;
+}
+
+int main() {
+    std::string test("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,,*47");
+    test +=        "\n$GPGGA,170834,4124.8963,N,08151.6838,W,1,05,1.5,280.2,M,-34.0,M,,,*59";
+
+    std::istringstream ss(test);
+    ss.unsetf(std::ios::skipws);
+    using namespace ranges::v3;
+    for (gga const& g : view::bounded(istream_range<gga>(ss))) {
+        std::cout << g << "\n";
+    }
+
+}
