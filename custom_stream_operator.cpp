0b3a308b27dab33ff0c21b30a6e9116d566d9efe
Jeff Trull <edaskel@att.net>
2015-08-24 16:26:18 -0700

    Separate boost::geometry point construction for readability

--- custom_stream_operator.cpp
+++ custom_stream_operator.cpp
@@ -28,22 +28,23 @@ int main() {
     using namespace boost::geometry;
     typedef model::point<double, 2, cs::geographic<degree> > coord_t;
     ss = istringstream(test);   // reset stream
+    auto construct_coord = [](gga_t const& g) {
+        double lat_deg = floor(g.latitude / 100.0);
+        if (g.lat_hemi == 'W') {
+            lat_deg = -lat_deg;
+        }
+        double long_deg = floor(g.longitude / 100.0);
+        if (g.lat_hemi == 'S') {
+            long_deg = -long_deg;
+        }
+        return coord_t(lat_deg, long_deg);
+    };
     auto coord_range =
         istream_range<gga_t>(ss) |
         ranges::view::remove_if([](gga_t const& g) {
                 return g.dilution > 1.0;
             }) |
-        ranges::view::transform([](gga_t const& g) {
-                double lat_deg = floor(g.latitude / 100.0);
-                if (g.lat_hemi == 'W') {
-                    lat_deg = -lat_deg;
-                }
-                double long_deg = floor(g.longitude / 100.0);
-                if (g.lat_hemi == 'S') {
-                    long_deg = -long_deg;
-                }
-                return coord_t(lat_deg, long_deg);
-            });
+        ranges::view::transform(construct_coord);
 
     // print out resulting Geometry coordinates (only one passes dilution test)
     ranges::for_each(coord_range,
